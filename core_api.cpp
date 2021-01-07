304b87c0e8267e1cf555c6a472510ac8c4115654
binyamin <benjamin.sarussi@gmail.com>
2021-01-07 21:50:30 +0200

    design stage

--- core_api.cpp
+++ core_api.cpp
@@ -4,9 +4,52 @@
 #include "sim_api.h"
 
 #include <stdio.h>
+#include <vector>
+#include <cassert>
+
+using std::vector;
+
+
+
+struct SingleThread{
+
+	int id;
+	vector<Instruction> program;
+	tcontext registersFile;
+	int pc;
+
+	explicit SingleThread(int id) : id(id) {
+
+		for(int i = 0; i< REGS_COUNT; i++){
+			registersFile.reg[i] = 0;
+		}
+		pc = 0;
+		uint32_t line = -1;
+		Instruction newInstruction;
+		do{
+			SIM_MemInstRead( ++line, &newInstruction, id);
+			program.push_back(newInstruction);
+			assert(newInstruction.opcode == program[line].opcode);
+		} while (program[line].opcode != CMD_HALT);
+	}
+};
+
+
+struct Core {
+
+	enum mtType{BLOCKED, FINE_GRAINED};
+	vector<SingleThread> threads;
+int cyclesNumber;
+int instructionsNumber;
+
+	Core(){} 
+};
+
+
 
 
 void CORE_BlockedMT() {
+
 }
 
 void CORE_FinegrainedMT() {
