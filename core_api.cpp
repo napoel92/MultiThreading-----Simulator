b0a748621be377503aaf489c4f4960e5c64c913a
binyamin <benjamin.sarussi@gmail.com>
2021-01-08 17:57:37 +0200

    done with core_Blocked

--- core_api.cpp
+++ core_api.cpp
@@ -9,6 +9,19 @@
 
 using std::vector;
 
+const int NO_PC = -1;
+const int IDLE = -2;
+const int TOTAL_HALT = -1;
+
+static int fineGrainedCycles = 0;
+static int blockedCycles = 0;
+
+static int blockedInstructions = 0;
+static int fineGrainedInstructions = 0;
+
+static vector<tcontext> RegisterFileBlocked
+static vector<tcontext> RegFileFineGrained;
+
 
 
 struct SingleThread{
@@ -17,13 +30,14 @@ struct SingleThread{
 	vector<Instruction> program;
 	tcontext registersFile;
 	int pc;
+	int wait;
 
-	explicit SingleThread(int id) : id(id) {
+	explicit SingleThread(int id) : id(id),pc(NO_PC),wait(0) {
 
 		for(int i = 0; i< REGS_COUNT; i++){
 			registersFile.reg[i] = 0;
 		}
-		pc = 0;
+		
 		uint32_t line = -1;
 		Instruction newInstruction;
 		do{
@@ -32,17 +46,60 @@ struct SingleThread{
 			assert(newInstruction.opcode == program[line].opcode);
 		} while (program[line].opcode != CMD_HALT);
 	}
+
+	cmd_opcode execute(){
+		//todo
+		return CMD_ADD;
+		//FIXME
+	}
 };
+
 
 
 struct Core {
 
 	enum mtType{BLOCKED, FINE_GRAINED};
-	vector<SingleThread> threads;
-int cyclesNumber;
-int instructionsNumber;
 
-	Core(){} 
+	mtType type;
+	vector<SingleThread> threads;
+	//vector<bool> isWait;
+
+	Core(mtType type) : type(type){
+		for (int i = 0; i < SIM_GetThreadsNum(); i++){
+			threads.push_back(SingleThread(i));
+			//isWait.push_back(false);
+		}
+	} 
+
+
+
+
+	 int IncrementThread(int current){
+		unsigned int countHalt=0, countWait=0, active=0;
+		for(SingleThread i : threads) active = (i.wait) ? (active) : (active+1) ;
+		bool isHalt = true;
+		bool isWait = true;
+		
+
+		while( (countHalt!=threads.size())  &&  (countWait!=active)  &&  ((isHalt)||(isWait)) ){
+			
+			if(type==FINE_GRAINED) (++current)%(threads.size());
+			SingleThread& thread = threads[current];
+			isHalt = (thread.program[thread.pc].opcode==CMD_HALT);
+			isWait = ( thread.wait > 0);
+			assert( ((isHalt)&&(isWait))==false );
+			
+			countHalt = ( isHalt ) ? ( countHalt+1 ) : ( countHalt );
+			countWait = ( isWait ) ? ( countWait+1 ) : ( countWait );
+			if( (type==BLOCKED) && (isWait || isHalt ) )  (current+1)%(threads.size());
+
+		}
+
+	assert( (countHalt!=threads.size())  ||  (countWait!=active) );
+	current = (countHalt==threads.size()) ? (TOTAL_HALT) : (current);
+	current = (countWait==active) ? (IDLE) : (current);
+	return current;
+	}
 };
 
 
@@ -50,6 +107,45 @@ int instructionsNumber;
 
 void CORE_BlockedMT() {
 
+	Core core(Core::BLOCKED);
+	struct Functor{
+		Core* core;
+		RegisterFileBlocked.resize(SIM_GetThreadsNum());
+		Functor(Core* core):core(core){}
+		void operator()(int cycles){
+			for(SingleThread i : core->threads) if( i.wait ) i.wait-=cycles;
+		}
+	};
+	
+
+	Functor decrementWait(&core);
+	int currentThred = 0;
+	while ( currentThred!=TOTAL_HALT ){
+	// performs another command every iteration 
+
+		++blockedCycles;
+		decrementWait(1);
+		cmd_opcode command = core.threads[currentThred].execute();
+		if( command==CMD_HALT ) RegisterFileBlocked[currentThred] = core.threads[currentThred].registersFile ;
+		
+		if( command==CMD_HALT || command==CMD_LOAD || command==CMD_STORE ){
+			int previous = currentThred;
+			currentThred = core.IncrementThread(currentThred);
+
+			while (currentThred==IDLE){
+				++blockedCycles;
+				decrementWait(1);
+				currentThred = core.IncrementThread(previous);
+			}
+
+
+			if( (previous!=currentThred) && (currentThred!=TOTAL_HALT) ){
+				int contextSwitchFine = SIM_GetSwitchCycles();
+				blockedCycles += contextSwitchFine;
+				decrementWait(contextSwitchFine);
+			}
+		}		
+	}	
 }
 
 void CORE_FinegrainedMT() {
